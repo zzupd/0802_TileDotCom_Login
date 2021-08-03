3876eee6ca1b2edef09765411bd60115dfcfe0da
zzupd <zzupd@naver.com>
2021-08-03 14:39:49 +0900

    0803_GNB구현완료

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
 $(function () {
 
+    // GNB 메뉴 blink(깜밖임 효과) 시작
+    setInterval(fnBlink, 580);
 
+    function fnBlink() {
+        $("#blinkStr").toggle();
+    }
+    // GNB 메뉴 blink(깜밖임 효과) 끝
 
 
 });
